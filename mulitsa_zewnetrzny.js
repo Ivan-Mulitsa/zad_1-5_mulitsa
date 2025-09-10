2b1b69622cb4c3cb2ec97d020e4ad00fd22a0614
Ivan-Mulitsa <mulitsaivan20@gmail.com>
2025-09-10 20:19:56 +0200

    Add files via upload

---
+++ mulitsa_zewnetrzny.js
@@ -0,0 +1,7 @@
+document.write("Mam numer w dzienniku 18"+"<br>"); 
+document.write("Pierwszy wiersz to 13 Tak Nie 6"+"<br>"); 
+document.write("Pierwszy wiersz to 2 Nie Tak 6"+"<br>"); 
+
+
+document.write("ostatnia modyfikacja strony".fontcolor("yellow").bold().fontsize(6)+"<br>");
+document.write(document.lastModified.fontcolor("silver").strike().fontsize(6));
